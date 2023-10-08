9d7278fe39d96ce0a1b6d92287291cd1d02cb0ec
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-08 15:16:59 +0200

    initial commit

---
+++ src/main/java/com/msik404/karmaappgateway/KarmaAppGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msik404.karmaappgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaAppGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmaAppGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msik404/karmaappgateway/KarmaAppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msik404.karmaappgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmaAppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
