e96fa5eb8bcb2c2729249d76bfedbf7e71f212dd
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-20 20:28:28 +0200

    Fixed filter authentication exception bug.

--- src/main/java/com/msik404/karmaappgateway/KarmaAppGatewayControllerAdvice.java
+++ src/main/java/com/msik404/karmaappgateway/KarmaAppGatewayControllerAdvice.java
@@ -2,13 +2,20 @@ package com.msik404.karmaappgateway;
 
 import com.msik404.karmaappgateway.exception.RestFromGrpcException;
 import com.msik404.karmaappgateway.grpc.client.exception.InternalRestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class KarmaAppGatewayControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ProblemDetail authenticationException(AuthenticationException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.getMessage());
+    }
 
     @ExceptionHandler(RestFromGrpcException.class)
     public ProblemDetail restFromGrpcException(RestFromGrpcException ex) {
--- src/main/java/com/msik404/karmaappgateway/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/msik404/karmaappgateway/auth/jwt/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import com.msik404.karmaappgateway.grpc.client.GrpcService;
 import com.msik404.karmaappgateway.user.Role;
 import com.msik404.karmaappgateway.user.exception.UserNotFoundException;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -15,10 +16,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -37,7 +42,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
-    ) throws ServletException, IOException, JwtException, IllegalArgumentException, UserNotFoundException {
+    ) throws ServletException, IOException, AuthenticationException {
 
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith(TOKEN_PREFIX)) {
@@ -45,28 +50,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         String jwt = authHeader.substring(TOKEN_PREFIX.length());
-        Claims claims = jwtService.extractAllClaims(jwt);
-        // 1. Get subject from token claims, null if something is wrong
-        // 2. Checks whether user is not already authenticated,
-        // this is useful when there are many authentication methods.
-        if (claims.getSubject() != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            // parse userId Long type represented as string to Long type
-            ObjectId userId = new ObjectId(claims.getSubject());
-            Role role = grpcService.findUserRole(userId);
-            var authentication = new UsernamePasswordAuthenticationToken(
-                    userId,
-                    null,
-                    List.of(new SimpleGrantedAuthority(role.name()))
-            );
-            // Adds interesting data like ip address and session id
-            authentication.setDetails(new WebAuthenticationDetails(request));
-            // Docs state that this is required for thread safety:
-            // https://docs.spring.io/spring-security/reference/servlet/authentication/architecture.html
-            SecurityContext context = SecurityContextHolder.createEmptyContext();
-            context.setAuthentication(authentication);
-            SecurityContextHolder.setContext(context);
+        try {
+            Claims claims = jwtService.extractAllClaims(jwt);
+            // 1. Get subject from token claims, null if something is wrong
+            // 2. Checks whether user is not already authenticated,
+            // this is useful when there are many authentication methods.
+            if (claims.getSubject() != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                // parse userId Long type represented as string to Long type
+                ObjectId userId = new ObjectId(claims.getSubject());
+                Role role = grpcService.findUserRole(userId);
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        userId,
+                        null,
+                        List.of(new SimpleGrantedAuthority(role.name()))
+                );
+                // Adds interesting data like ip address and session id
+                authentication.setDetails(new WebAuthenticationDetails(request));
+                // Docs state that this is required for thread safety:
+                // https://docs.spring.io/spring-security/reference/servlet/authentication/architecture.html
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                context.setAuthentication(authentication);
+                SecurityContextHolder.setContext(context);
+            }
+            filterChain.doFilter(request, response);
+
+        } catch (ExpiredJwtException ex) {
+            throw new CredentialsExpiredException(ex.getMessage());
+        } catch (JwtException | IllegalArgumentException ex) {
+            throw new BadCredentialsException(ex.getMessage());
+        } catch (UserNotFoundException ex) {
+            throw new UsernameNotFoundException(ex.getMessage());
         }
-        filterChain.doFilter(request, response);
     }
 
 }
