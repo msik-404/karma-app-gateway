9877741095a86fb650f84d22f08487d00c7738f9
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-20 15:36:54 +0200

    Changed error message so that fits better to gateway.

--- src/main/java/com/msik404/karmaappgateway/user/exception/UserNotFoundException.java
+++ src/main/java/com/msik404/karmaappgateway/user/exception/UserNotFoundException.java
@@ -10,7 +10,7 @@ import org.springframework.lang.NonNull;
 public class UserNotFoundException extends RestFromGrpcException {
 
     public static final String Id = "UserNotFound";
-    private static final String ERROR_MESSAGE = "Requested UserDocument was not found.";
+    private static final String ERROR_MESSAGE = "User was not found.";
 
     public UserNotFoundException() {
         super(ERROR_MESSAGE);
