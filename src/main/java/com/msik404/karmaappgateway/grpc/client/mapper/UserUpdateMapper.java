1f196dc6d8a6651473b7ae1b9d8e9ff4bebc9259
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-22 20:19:23 +0200

    Implemented mapper class for mapping update classes to update grpc request.

---
+++ src/main/java/com/msik404/karmaappgateway/grpc/client/mapper/UserUpdateMapper.java
@@ -0,0 +1,90 @@
+package com.msik404.karmaappgateway.grpc.client.mapper;
+
+import java.util.Optional;
+
+import com.msik404.karmaappgateway.grpc.client.exception.UnsupportedRoleException;
+import com.msik404.karmaappgateway.user.dto.UserUpdateRequestWithAdminPrivilege;
+import com.msik404.karmaappgateway.user.dto.UserUpdateRequestWithUserPrivilege;
+import com.msik404.karmaappusers.grpc.UpdateUserRequest;
+import org.bson.types.ObjectId;
+import org.springframework.lang.NonNull;
+
+public class UserUpdateMapper {
+
+    @NonNull
+    private static Optional<UpdateUserRequest.Builder> mapImpl(
+            @NonNull ObjectId userId,
+            @NonNull UserUpdateRequestWithUserPrivilege request
+    ) {
+
+        var builder = UpdateUserRequest.newBuilder();
+        builder.setUserId(MongoObjectIdMapper.mapToUsersMongoObjectId(userId));
+
+        boolean somethingWasSet = false;
+
+        if (request.firstName() != null) {
+            somethingWasSet = true;
+            builder.setFirstName(request.firstName());
+        }
+        if (request.lastName() != null) {
+            somethingWasSet = true;
+            builder.setLastName(request.lastName());
+        }
+        if (request.username() != null) {
+            somethingWasSet = true;
+            builder.setUsername(request.username());
+        }
+        if (request.email() != null) {
+            somethingWasSet = true;
+            builder.setEmail(request.email());
+        }
+        if (request.password() != null) {
+            somethingWasSet = true;
+            builder.setPassword(request.password());
+        }
+
+        if (somethingWasSet) {
+            return Optional.of(builder);
+        }
+        return Optional.empty();
+    }
+
+    @NonNull
+    public static Optional<UpdateUserRequest> map(
+            @NonNull ObjectId userId,
+            @NonNull UserUpdateRequestWithUserPrivilege request
+    ) {
+        return mapImpl(userId, request).map(UpdateUserRequest.Builder::build);
+    }
+
+    @NonNull
+    public static Optional<UpdateUserRequest> map(
+            @NonNull ObjectId userId,
+            @NonNull UserUpdateRequestWithAdminPrivilege request
+    ) throws UnsupportedRoleException {
+
+        boolean somethingWasSet = false;
+
+        UpdateUserRequest.Builder builder;
+        if (request.userUpdateByUser() != null) {
+            Optional<UpdateUserRequest.Builder> optionalBuilder = mapImpl(userId, request.userUpdateByUser());
+            if (optionalBuilder.isPresent()) {
+                somethingWasSet = true;
+                builder = optionalBuilder.get();
+            } else {
+                builder = UpdateUserRequest.newBuilder();
+            }
+        } else {
+            builder = UpdateUserRequest.newBuilder();
+        }
+
+        if (request.role() != null) {
+            builder.setRole(RoleMapper.map(request.role()));
+        }
+
+        if (somethingWasSet) {
+            return Optional.of(builder.build());
+        }
+        return Optional.empty();
+    }
+}
