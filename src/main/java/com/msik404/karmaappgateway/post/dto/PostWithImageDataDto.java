7d3c43cfe4753a52a9f7682908f07c77db7f28fb
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-27 14:07:23 +0200

    Forgot about @Nullable annotation to byte[]

--- src/main/java/com/msik404/karmaappgateway/post/dto/PostWithImageDataDto.java
+++ src/main/java/com/msik404/karmaappgateway/post/dto/PostWithImageDataDto.java
@@ -1,7 +1,7 @@
 package com.msik404.karmaappgateway.post.dto;
 
 import lombok.NonNull;
+import org.springframework.lang.Nullable;
 
-public record PostWithImageDataDto(@NonNull PostDto postDto, byte[] imageData) {
-
+public record PostWithImageDataDto(@NonNull PostDto postDto, @Nullable byte[] imageData) {
 }
